022654231235ca69711dd19d909e79ca9fc9f139
Jeremiah Biard <jeremiahbiard@gmail.com>
2018-05-13 12:10:27 -0400

    add test for parseRDF and create function to make it pass

---
+++ databases/test/parse-rdf-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fs = require('fs');
+const expect = require('chai').expect;
+const parseRDF = require('../lib/parse-rdf');
+
+const rdf = fs.readFileSync(`${__dirname}/pg132.rdf`);
+
+describe('parseRDF', () => {
+    it('should be a function', () => {
+        expect(parseRDF).to.be.a('function');
+    });
+});
